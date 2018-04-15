32681644d6860a838b117bcf4e41c0ff6206814a
Maxime <maxime.gourgoulhon@gmail.com>
2018-04-15 12:50:17 +0200

    Update sql : children diet

--- SQL/deploy.sql
+++ SQL/deploy.sql
@@ -1,6 +1,26 @@
 set serveroutput on format wrapped;
 
 BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE ACVL_ChildDiet';
+    DBMS_OUTPUT.put_line('DROP TABLE ACVL_ChildDiet');
+EXCEPTION
+WHEN OTHERS THEN
+   IF SQLCODE != -942 THEN
+      RAISE;
+   END IF;
+END;
+/
+BEGIN
+    EXECUTE IMMEDIATE 'DROP TABLE ACVL_Diet';
+    DBMS_OUTPUT.put_line('DROP TABLE ACVL_Diet');
+EXCEPTION
+WHEN OTHERS THEN
+   IF SQLCODE != -942 THEN
+      RAISE;
+   END IF;
+END;
+/
+BEGIN
     EXECUTE IMMEDIATE 'DROP TABLE ACVL_family';
     DBMS_OUTPUT.put_line('DROP TABLE ACVL_family');
 EXCEPTION
@@ -52,6 +72,8 @@ CREATE TABLE ACVL_Children (
     id number(6) DEFAULT ACVL_Children_id_seq.nextval PRIMARY KEY,
     firstname VARCHAR2(32) NOT NULL,
     lastname VARCHAR2(64) NOT NULL,
+    gender CHAR(1) NOT NULL,
+    grade CHAR(3) NOT NULL,
     birthdate DATE NOT NULL
 );
 
@@ -63,8 +85,26 @@ CREATE TABLE ACVL_Family (
     FOREIGN KEY (idChild) references ACVL_Children(id)
 );
 
+CREATE TABLE ACVL_Diet (
+    diet varchar(255) PRIMARY KEY
+);
+
+CREATE TABLE ACVL_ChildDiet (
+    diet varchar(255),
+    idChild number(6),
+    PRIMARY KEY (diet, idChild),
+    FOREIGN KEY (diet) references ACVL_Diet(diet),
+    FOREIGN KEY (idChild) references ACVL_Children(id)
+);
+
 select * from ACVL_CHILDREN;
 select * from ACVL_Family;
 SELECT * FROM ACVL_Users u, ACVL_Children c, ACVL_family f where u.username = f.username and f.idChild = c.id and u.username = 'maxime';
+insert into ACVL_DIET values ('végétarien');
+insert into ACVL_DIET values ('sans gluten');
+
+select * from ACVL_ChildDiet;
+
+SELECT * FROM ACVL_Diet;
 
 --SELECT ACVL_Children_id_seq.next FROM dual;
