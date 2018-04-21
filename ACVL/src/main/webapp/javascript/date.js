2f08d49c3e86faf04a2671394d518ddb1eb9294f
Julie Saouli <Julie.Saouli@ensimag.grenoble-inp.fr>
2018-04-21 17:34:15 +0200

    check date via js + error msg

--- ACVL/src/main/webapp/javascript/date.js
+++ ACVL/src/main/webapp/javascript/date.js
@@ -1,4 +1,5 @@
 function isDate(dateString) {
+    console.log(dateString);
     // Format : YYYY-MM-DD
     var datePattern = /^\d{4}-\d{2}-\d{2}$/;
     if (!dateString.match(datePattern))
@@ -7,6 +8,19 @@ function isDate(dateString) {
     var d = new Date(dateString);
     if (!d.getTime() && d.getTime() !== 0)
         return false;   
-    //return d.toISOString().slice(0,10) === dateString;
-    return false;
+    return d.toISOString().slice(0,10) === dateString;
+}
+
+function checkBirthdate() {
+    var birthdate = document.getElementsByName("birthdate")[0];
+    if (isDate(birthdate.value)) {
+        return true;
+    } else {
+        var p = document.createElement("p");
+        p.setAttribute("id", "errMsg");
+        var txt = document.createTextNode("Date invalide : Veuillez respecter le format YYYY-MM-DD.");
+        p.appendChild(txt);
+        birthdate.parentElement.appendChild(p);
+        return false;
+    }
 }
